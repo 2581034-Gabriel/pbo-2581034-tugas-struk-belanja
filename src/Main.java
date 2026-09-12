51e622689eb69226f300cb62b579b4566e9b94b2
Vivaldhy Gabriel Valentino P.Silitonga <2581034@unai.edu>
2026-09-12 22:40:21 +0700

    Barang 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nama Barang ke-1: ");
+        String nama1 = scanner.nextLine();
+
+        System.out.print("Harga: ");
+        int harga1 = scanner.nextInt();
+
+        System.out.print("Jumlah: ");
+        int jumlah1 = scanner.nextInt();
+
+    }
+
+
+
+}
