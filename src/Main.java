0bcd3bcdd81559cc0ba2876b47c9855158641d8e
Vivaldhy Gabriel Valentino P.Silitonga <2581034@unai.edu>
2026-09-12 22:43:59 +0700

    Barang 2 dan 3

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,30 @@ public class Main {
         System.out.print("Jumlah: ");
         int jumlah1 = scanner.nextInt();
 
+        scanner.nextLine();
+
+        System.out.print("Nama Barang ke-2: ");
+        String nama2 = scanner.nextLine();
+
+        System.out.print("Harga: ");
+        int harga2 = scanner.nextInt();
+
+        System.out.print("Jumlah: ");
+        int jumlah2 = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Nama Barang ke-3: ");
+        String nama3 = scanner.nextLine();
+
+        System.out.print("Harga: ");
+        int harga3 = scanner.nextInt();
+
+        System.out.print("Jumlah: ");
+        int jumlah3 = scanner.nextInt();
+
+
+
     }
 
 
